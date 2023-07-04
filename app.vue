d94421bc8faaa58bdc89033d28787047ba244813
Tom Newton <newton.thomasjames@gmail.com>
2023-07-04 14:15:35 +0100

    progress

--- app.vue
+++ app.vue
@@ -1,5 +1,138 @@
+<script setup lang="ts">
+import { PaintBrushIcon, SparklesIcon, WrenchScrewdriverIcon } from '@heroicons/vue/24/outline'
+
+useHead({
+  htmlAttrs: {
+    class: 'h-full',
+  },
+  bodyAttrs: {
+    class: 'h-full font-sans antialiased text-slate-900',
+  },
+})
+
+const formState = ref<'idle' | 'processing' | 'done' | 'error'>('idle')
+
+function attemptJoinWaitlist() {
+  formState.value = 'processing'
+}
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <section class="overflow-hidden bg-slate-50 border-b border-slate-200 relative">
+    <div
+      class="absolute inset-y-0 hidden w-full min-w-[1360px] bg-[url('/beams.png')] bg-[length:1200px_700px] bg-[position:calc(50%_+_230px)_-50px] bg-no-repeat lg:block pointer-events-none"
+    ></div>
+    <div class="max-w-[1350px] mx-auto px-8 pb-24 pt-[120px]">
+      <div class="flex w-full items-center">
+        <div class="max-w-[650px] md:min-w-[650px] mx-auto">
+          <h1 class="font-extrabold text-5xl text-slate-950 leading-[50px]">
+            Create a beautiful, modern adviser website with complete ease.
+          </h1>
+
+          <p class="leading-[30px] text-slate-600 mt-4 max-w-[620px] text-lg">
+            Stun your clients, convert more leads and supercharge your online presence. A brand new platform designed
+            for financial advisers and created by experts in web design, accessibility and SEO.
+          </p>
+
+          <form @submit.prevent="attemptJoinWaitlist" class="mt-8 max-w-[600px]">
+            <label for="email" class="block font-semibold mb-2 text-sm text-slate-800">
+              Join the waitlist for early access:
+            </label>
+
+            <div class="flex items-center space-x-3">
+              <input
+                id="email"
+                type="email"
+                class="text-sm sm:text-[1rem] text-slate-800 leading-5 px-4 py-3 rounded-lg shadow border border-slate-200 flex-1"
+                placeholder="email@company.co.uk"
+                autofocus
+                required
+              />
+
+              <button
+                type="submit"
+                class="text-sm rounded-lg leading-5 px-4 py-3 bg-slate-900 text-white border border-slate-900 font-semibold shadow inline-flex items-center justify-center appearance-none"
+              >
+                <span>Join the waitlist</span>
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div class="hidden lg:flex">
+          <div class="relative flex-shrink-0 p-3">
+            <div class="relative z-10 rounded-lg ring-1 ring-slate-900/5 shadow-xl overflow-hidden">
+              <img src="/best-ifa-1.png" alt="" class="h-[369px] w-[320px]" />
+            </div>
+          </div>
+
+          <div class="relative flex-shrink-0 mt-14 p-3">
+            <div class="relative z-10 rounded-lg ring-1 ring-slate-900/5 shadow-xl overflow-hidden pb-8 bg-white">
+              <img src="/sterling-wisdom-1.png" alt="" class="h-[337px] w-[320px]" />
+            </div>
+          </div>
+
+          <div class="relative flex-shrink-0 p-3">
+            <div class="relative z-10 rounded-lg ring-1 ring-slate-900/5 shadow-xl overflow-hidden pb-8 bg-white">
+              <img src="/watson-smith-1.png" alt="" class="h-[337px] w-[320px]" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex items-center gap-8 mt-16">
+        <div class="w-1/3 flex items-start">
+          <div
+            class="mr-6 w-[38px] h-[38px] shadow bg-blue-50 border border-blue-600 rounded-lg flex items-center justify-center"
+          >
+            <WrenchScrewdriverIcon class="w-6 h-6 text-blue-600" />
+          </div>
+
+          <div class="flex-1">
+            <div class="font-semibold">Beautiful hand-crafted templates</div>
+            <p class="text-slate-500 mt-3 leading-6">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, voluptate provident? Tenetur, velit id
+              libero.
+            </p>
+          </div>
+        </div>
+
+        <div class="w-1/3 flex items-start">
+          <div
+            class="mr-6 w-[38px] h-[38px] shadow bg-blue-50 border border-blue-600 rounded-lg flex items-center justify-center"
+          >
+            <PaintBrushIcon class="w-6 h-6 text-blue-600" />
+          </div>
+
+          <div class="flex-1">
+            <div class="font-semibold">Easy to customise to match your brand</div>
+
+            <p class="text-slate-500 mt-3 leading-6">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, voluptate provident? Tenetur, velit id
+              libero.
+            </p>
+          </div>
+        </div>
+
+        <div class="w-1/3 flex items-start">
+          <div
+            class="mr-6 w-[38px] h-[38px] shadow bg-blue-50 border border-blue-600 rounded-lg flex items-center justify-center"
+          >
+            <SparklesIcon class="w-6 h-6 text-blue-600" />
+          </div>
+
+          <div class="flex-1">
+            <div class="font-semibold">Use our content examples, or use AI</div>
+
+            <p class="text-slate-500 mt-3 leading-6">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, voluptate provident? Tenetur, velit id
+              libero.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="bg-white"></section>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,12 @@ module.exports = {
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
+    './App.vue',
   ],
   theme: {
+    fontFamily: {
+      sans: ['"Inter var"', 'Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
