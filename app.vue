11b0f7d6c405281a5957700d2a4d4d4be02a0f8a
Tom Newton <newton.thomasjames@gmail.com>
2023-07-05 18:48:39 +0100

    wip

--- app.vue
+++ app.vue
@@ -53,6 +53,13 @@ onUnmounted(() => document.removeEventListener('scroll', listener))
 
 const selectedPreview = ref<'modern' | 'corporate' | 'family'>('modern')
 const mobileMenuOpen = ref(false)
+
+const waitlist = ref(null)
+
+function focusWaitlistInput() {
+  mobileMenuOpen.value = false
+  waitlist.value?.focus()
+}
 </script>
 
 <template>
@@ -93,6 +100,7 @@ const mobileMenuOpen = ref(false)
       <div class="px-5 py-4">
         <button
           type="button"
+          @click="focusWaitlistInput"
           class="flex w-full items-center justify-center appearance-none rounded-lg font-semibold text-blue-600 border-2 border-blue-600 px-3 py-2"
         >
           Join the waitlist
@@ -126,6 +134,7 @@ const mobileMenuOpen = ref(false)
 
         <button
           type="button"
+          @click="focusWaitlistInput"
           class="rounded-lg leading-5 text-sm font-semibold bg-transparent appearance-none border-2 border-blue-600 px-3 py-2 text-blue-600"
         >
           Join the waitlist
@@ -163,6 +172,7 @@ const mobileMenuOpen = ref(false)
             <div class="md:flex md:items-center md:space-x-4">
               <input
                 id="email"
+                ref="waitlist"
                 type="email"
                 class="text-sm sm:text-[1rem] text-slate-800 leading-5 px-4 py-3 rounded-lg shadow border border-slate-200 flex-1 outline-none focus:ring-2 ring-blue-500 focus:border-blue-500 w-full md:w-auto"
                 placeholder="email@company.co.uk"
